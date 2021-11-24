2fb3f6507d5f9889b0d30e8a9382822d7eee55e9
Rega Halma Ruzty <rega.hruzty@sproutasia.com>
2021-11-24 08:14:43 +0700

    update layout

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Rega Halma Ruzty'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'dev.rhr'
 
 export default function Layout({ children, home }) {
   return (
