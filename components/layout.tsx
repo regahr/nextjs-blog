10cd72801c2af247b6b766e0b5a99f6f94f76350
Rega Halma Ruzty <rega.hruzty@sproutasia.com>
2021-11-24 13:16:38 +0700

    change name

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Rega Halma Ruzty'
+const name = 'Changed Name'
 export const siteTitle = 'dev.rhr'
 
 export default function Layout({
