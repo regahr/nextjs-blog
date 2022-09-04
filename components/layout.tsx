8a2595d62b34bb8d5a707b7cc4d26c6527946c52
Rega HR <90163540+regasprout@users.noreply.github.com>
2022-09-05 05:56:45 +0700

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Changed Name'
+const name = 'Rega Halma Ruzty'
 export const siteTitle = 'dev.rhr'
 
 export default function Layout({
